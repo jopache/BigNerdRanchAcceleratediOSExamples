0692cd65acb5a7e02840e403495c7c774694d28d
Jose-Pacheco <jose.pacheco@twinspires.com>
2018-08-14 17:15:16 -0400

    ch 9: Implemented delete ability, add ability, removed auto creation of 5 entries

--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -17,11 +17,37 @@ class ItemsViewController: UITableViewController {
     }
     
     @IBAction func addNewItem(_ sender: UIButton) {
+//        let lastRow = tableView.numberOfRows(inSection: 0)
+//        let indexPath = IndexPath(row: lastRow, section: 0)
+//
+//        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+        
         
     }
     
     @IBAction func toggleEditingMode(_ sender: UIButton) {
-        
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: UIControlState.normal)
+            setEditing(true, animated: true)
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let item = itemStore.allItems[indexPath.row]
+            itemStore.removeItem(item)
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+        }
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -30,7 +56,6 @@ class ItemsViewController: UITableViewController {
         let item = itemStore.allItems[indexPath.row]
         cell.textLabel?.text = item.name
         cell.detailTextLabel?.text = "$\(item.valueInDollars)"
-        
         return cell
     }
 }
