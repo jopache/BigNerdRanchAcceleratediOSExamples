74f420069033480813157496122c3a574d71aeff
Jose-Pacheco <jose.pacheco@twinspires.com>
2018-08-15 10:16:19 -0400

    ch 10: Added custom cell for homepwner

---
+++ Homepwner/Homepwner/ItemCell.swift
@@ -0,0 +1,15 @@
+//
+//  ItemCell.swift
+//  Homepwner
+//
+//  Created by Jose Pacheco on 8/15/18.
+//  Copyright © 2018 Jose Pacheco. All rights reserved.
+//
+
+import UIKit
+
+class ItemCell: UITableViewCell {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var serialNumberLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
--- Homepwner/Homepwner/ItemsViewController.swift
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -9,37 +9,15 @@
 import UIKit
 
 class ItemsViewController: UITableViewController {
+    
+    // MARK: Instance Variables
     var itemStore: ItemStore!
     
+    
+    // MARK: Overridden methods
     override func tableView(_ tableView: UITableView,
                             numberOfRowsInSection section: Int) -> Int {
         return itemStore.allItems.count
-    }
-    
-    @IBAction func addNewItem(_ sender: UIButton) {
-//        let lastRow = tableView.numberOfRows(inSection: 0)
-//        let indexPath = IndexPath(row: lastRow, section: 0)
-//
-//        tableView.insertRows(at: [indexPath], with: .automatic)
-        
-        let newItem = itemStore.createItem()
-        if let index = itemStore.allItems.index(of: newItem) {
-            let indexPath = IndexPath(row: index, section: 0)
-            
-            tableView.insertRows(at: [indexPath], with: .automatic)
-        }
-        
-        
-    }
-    
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        if isEditing {
-            sender.setTitle("Edit", for: .normal)
-            setEditing(false, animated: true)
-        } else {
-            sender.setTitle("Done", for: UIControlState.normal)
-            setEditing(true, animated: true)
-        }
     }
     
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
@@ -52,14 +30,39 @@ class ItemsViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
-        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath) as! ItemCell
         let item = itemStore.allItems[indexPath.row]
-        cell.textLabel?.text = item.name
-        cell.detailTextLabel?.text = "$\(item.valueInDollars)"
+        cell.nameLabel.text = item.name
+        cell.valueLabel.text = "$\(item.valueInDollars)"
+        cell.serialNumberLabel.text = item.serialNumber
         return cell
     }
     
     override func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
         itemStore.moveItem(from: sourceIndexPath.row, to: destinationIndexPath.row)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.rowHeight = 65
+    }
+
+    // MARK: Actions
+    @IBAction func addNewItem(_ sender: UIButton) {
+        let newItem = itemStore.createItem()
+        if let index = itemStore.allItems.index(of: newItem) {
+            let indexPath = IndexPath(row: index, section: 0)
+            tableView.insertRows(at: [indexPath], with: .automatic)
+        }
+    }
+    
+    @IBAction func toggleEditingMode(_ sender: UIButton) {
+        if isEditing {
+            sender.setTitle("Edit", for: .normal)
+            setEditing(false, animated: true)
+        } else {
+            sender.setTitle("Done", for: UIControlState.normal)
+            setEditing(true, animated: true)
+        }
     }
 }
